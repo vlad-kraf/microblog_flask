bf42bd7b9975c22a806c863bf9e31a913c7908c2
vlad-kraf <vladkravchenkomail@gmail.com>
2019-06-23 10:54:34 +0300

    adding templates in project

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+# -*- coding: utf-8 -*-
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World! + Привет Мир!"
+    user = {'username': 'Miguel'}
+    return render_template('index.html', title='Home', user=user)
