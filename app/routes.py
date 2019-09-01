e5f81f41dcf966189955e9f5991d7347f18efe46
vlad-kraf <vladkravchenkomail@gmail.com>
2019-09-01 18:43:52 +0300

    added last_seen tracking in routes on every request from user. fix

--- app/routes.py
+++ app/routes.py
@@ -85,7 +85,7 @@ def user(username):
 
 
 @app.before_request
-def before_reauest():
+def before_request():
     if current_user.is_authenticated:
         current_user.last_seen = datetime.utcnow()
         db.session.commit()
